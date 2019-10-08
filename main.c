e44e59f24690bff2b666a1d49160d5c7cbe02f01
1813252 <55134827+1813252@users.noreply.github.com>
2019-10-08 15:12:07 +0900

    01

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+
+	int meter,i=0;
+	
+	
+	
+	while(i<3)
+	{
+		meter=i*1609;
+		printf("%d 마일은 %d미터 입니다.\n",i,meter);
+		i++;
+	}
+	
+	return 0;
+}
